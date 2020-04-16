af0aad18e6929a043616ce3b6adcf370abbda16a
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2020-04-16 07:09:43 -0300

    change error message

--- app/models/Mysql.php
+++ app/models/Mysql.php
@@ -13,7 +13,7 @@
                 self::$pdo = new PDO('mysql:host='.HOST.';dbname='.DATABASE,USER,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
                 self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
               }catch(Exception $e) {
-                echo 'Erro ao se conectar';
+                echo 'connect error';
               }
             }
 
