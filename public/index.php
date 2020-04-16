fa96883a0052de15c4a4bc81274a2cd13b268a7b
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2020-04-16 04:29:52 -0300

    register page/front-end complete

--- public/index.php
+++ public/index.php
@@ -23,15 +23,29 @@
 <div class="container">
     <div class="row">
 
-    <?php for($i = 0; $i < 10; $i++) {?>
-
     <div class="col-sm-4">
       <img src="img/img1.png" alt="sky" class="img-fluid">
     </div><!--col-sm-->
 
-    <?php } ?>
   </div><!--row-->
 </div><!--container-->
+
+<br>
+
+<div class="line"></div>
+
+<footer>
+    <h1>Register Photo</h1>
+    <div class="container">
+        <form method="POST" id="form-register" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="">Image</label>
+                <input type="file" class="form-control" name="image">
+            </div><!--form-group-->
+        </form>
+        <button type="submit" class="btn btn-primary">Register!</button>
+    </div><!--container-->
+</footer>
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
